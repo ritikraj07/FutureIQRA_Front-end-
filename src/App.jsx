dd8d6dd5aacc556da2240231e0f56d2ebdb21f58
ritikraj07 <ritikra3rrr@gmail.com>
2024-02-02 13:48:31 +0530

    icon meta tag update

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,9 @@ function App() {
         <title>Future IQRA</title>
         <meta
           name="description"
-          content="Unlock your potential with our Professional Skill Development Program, tailored for career growth. Navigate daily life effortlessly with our insightful Life Hack Tips and Tricks. Join us in the pursuit of excellence, mastering skills for success and simplifying everyday challenges."
+          content="Unlock your potential with our Professional Skill Development Program. Join us for career growth and navigate daily life effortlessly with insightful Life Hack Tips and Tricks."
         />
-        <link
-          rel="canonical"
-          type="image/png"
-          sizes="512x512"
-          href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
-        />
+
         <link
           rel="shortcut icon"
           type="image/png"
@@ -35,29 +30,6 @@ function App() {
           rel="icon"
           type="image/png"
           sizes="512x512"
-          href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
           href="https://65b51b3151be0ca5adcbbb85--joyful-kheer-008761.netlify.app/Accets/favicon.png"
         />
       </Helmet>
